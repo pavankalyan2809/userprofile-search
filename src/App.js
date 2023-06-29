8debabd36380701abe46b0b018ee39bf3cbdf6b6
pavankalyan2809 <pavankalyanbhuthagaddala@gmail.com>
2023-06-29 12:54:30 +0000

    Erros during practice

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import {Component} from 'react'
+import UserProfile from './components/UserProfile'
+
+import './App.css'
+
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Esther Howard',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Floyd Miles',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Jacob Jones',
+    role: 'Software Developer',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'Devon Lane',
+    role: 'Software Developer',
+  },
+]
+
+class App extends Component {
+  state = {
+    searchInput: '',
+  }
+  onChangeSearchInput = event => {
+    this.setState({
+      searchInput: event.target.value,
+    })
+  }
+  render() {
+    const {searchInput} = this.state
+    return (
+      <div className="app-container">
+        <h1 className="title">Users List</h1>
+        <input type="search" onChange={this.onChangeSearchInput} />
+        <ul className="list-container">
+          {userDetailsList.map(eachUser => (
+            <UserProfile userDetails={eachUser} key={eachUser.uniqueNo} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App
